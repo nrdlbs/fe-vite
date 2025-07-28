69e6a6fb9cd70e1f9107c2fb7d5042315429f80f
tienlevi <nguyentrachtien2401@gmail.com>
2025-07-28 14:28:35 +0700

    test commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import ItemList from '@/components/NestedRerender/ItemList'
 
 export default function Home() {
+  const name = 'Tien'
   return (
     <div className='space-y-8'>
       <ItemList />
